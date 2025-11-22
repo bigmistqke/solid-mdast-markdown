6de5c809b47080e982cbc48b3f9ca65e6def7fa5
bigmistqke <vandijckv@gmail.com>
2025-11-22 16:53:49 +0100

    dev: cleanup App

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,14 +1,12 @@
 import { Route, Router } from '@solidjs/router'
-import { createEffect, createSignal, type Component } from 'solid-js'
+import { type Component } from 'solid-js'
 import { TestViewer } from './TestViewer'
 
 const App: Component = () => {
   return (
-    <>
-      <Router base={import.meta.env.VITE_BASE ?? './'}>
-        <Route path="/" component={TestViewer} />
-      </Router>
-    </>
+    <Router base={import.meta.env.VITE_BASE ?? './'}>
+      <Route path="/" component={TestViewer} />
+    </Router>
   )
 }
 
